1814eed73f89a2017436f325a19bd8fc9637f68e
hiliyongke <249191296@qq.com>
2022-11-11 07:22:53 +0000

    Deploying to gh-pages from @ hiliyongke/notes@ce868f366f1e9063da3538a1dcd4e0b068dfa209 🚀

---
+++ assets/index.md.50e13f4b.js
@@ -0,0 +1,10 @@
+import{_ as l,r as i,e as d,f as u,o as p,c as r,b as a,u as h,p as m,g as f,h as v}from"./app.6fbfbcb1.js";const _=e=>(m("data-v-33d05871"),e=e(),f(),e),g=_(()=>a("h3",{class:"title"},"\u81F4\u81EA\u5DF1",-1)),x=["innerHTML"],y=_(()=>a("p",{align:"center"},[a("a",{class:"start",href:"/notes/01-\u57FA\u7840\u6280\u80FD/index"}," \u626C\u5E06\u8D77\u822A "),a("a",{class:"github",href:"https://github.com/hiliyongke",target:"_blank"}," Github ")],-1)),I={__name:"index",setup(e){let s=i(""),n=0;return d(()=>{let o=`
+    <p>\u6700\u6015\u4F60\u4E00\u751F\u788C\u788C\u65E0\u4E3A\uFF0C\u8FD8\u5B89\u6170\u81EA\u5DF1\u5E73\u51E1\u53EF\u8D35\u3002</p>
+    <p>\u4E0D\u8981\u60E7\u6015\u5199\u51FA\u4E0D\u5B8C\u7F8E\u7684\u4EE3\u7801\uFF0C\u53EA\u8981\u5728\u540E\u7EED\u8FED\u4EE3\u8FC7\u7A0B\u4E2D\u201C\u89C1\u62DB\u62C6\u62DB\u201D\uFF0C\u4EE3\u7801\u5C31\u4F1A\u53D8\u5F97\u8D8A\u6765\u8D8A\u5B8C\u5584\uFF0C\u6846\u67B6\u4E5F\u4F1A\u53D8\u5F97\u8D8A\u6765\u8D8A\u5065\u58EE\u3002</p>
+    <h3>\u4E3B\u8981\u5185\u5BB9</h3>
+    <p>\u8BB0\u5F55 JavaScript \u7684\u5404\u79CD\u65B9\u6CD5</p>
+    <p>Vue2 \u548C Vue3 \u7684\u4E00\u4E9B\u4F7F\u7528</p>
+    <p>Vue \u5468\u8FB9\u4E00\u4E9B\u751F\u6001</p>
+    <p>TypeScript \u57FA\u7840</p>
+    <p>\u6587\u7AE0\u6458\u5F55</p>
+    <p>\u524D\u7AEF\u9762\u8BD5\u9898</p>`.trim(),t="",c=0;n=setInterval(()=>{t.length<o.length?(t+=o[c++],s.value=t):(clearInterval(n),s.value=t)},100)}),u(()=>{clearInterval(n)}),(o,t)=>(p(),r("main",null,[g,a("p",{innerHTML:h(s)},null,8,x),y]))}},b=l(I,[["__scopeId","data-v-33d05871"]]),V=JSON.parse('{"title":"Lyk","description":"","frontmatter":{"layout":"home","title":"Lyk"},"headers":[],"relativePath":"index.md"}'),k={name:"index.md"},B=Object.assign(k,{setup(e){return(s,n)=>(p(),r("div",null,[v(b)]))}});export{V as __pageData,B as default};
